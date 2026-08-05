09d9115826a6bd8c6daac209b25d6b62ced6383b
zowork <sdkwork@zowalk.com>
2026-08-05 10:17:38 +0800

    chore: update 6 files
    Changed: apps/sdkwork-video-pc/packages/sdkwork-video-pc-generation/package.json, apps/sdkwork-video-pc/src/App.tsx, apps/sdkwork-video-pc/src/vite-env.d.ts, apps/sdkwork-video-pc/tsconfig.json, pnpm-lock.yaml, ... (6 total)

--- apps/sdkwork-video-pc/src/App.tsx
+++ apps/sdkwork-video-pc/src/App.tsx
@@ -1,10 +1,11 @@
 import { AuthGate } from './AuthGate';
-import { bootstrap } from './bootstrap/runtime';
+import { runtime } from './bootstrap/runtime';
 
 export default function App() {
   return (
     <AuthGate>
       <div className="app">
+        <span data-sdkwork-runtime={runtime().family} />
         <h1>SDKWork Video PC</h1>
       </div>
     </AuthGate>
